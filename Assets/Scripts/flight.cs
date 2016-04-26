ea1cc3f4eb381884eb7339396259762f30bd8436
Sungkeun Choi <soliven25@gmail.com>
2016-04-26 15:19:56 +0900

    add fire bullet

--- Assets/Scripts/flight.cs
+++ Assets/Scripts/flight.cs
@@ -5,6 +5,7 @@ public class flight : MonoBehaviour {
 
 	// Use this for initialization
 	public float speed;
+	public GameObject bulletPrefab;
 	void Start () {
 		speed = 0.06F;
 	}
@@ -16,6 +17,11 @@ public class flight : MonoBehaviour {
 		
 		transform.Translate(Vector3.right*speed*leftRight,Space.World);
 		transform.Translate(Vector3.up*speed*upDown, Space.World);
+		
+		if(Input.GetKeyDown("space")){
+			Instantiate(bulletPrefab, transform.position, transform.rotation);
+			Debug.Log("space pressed");
+		}
 		
 	}
 }
