329f3ff4f0cfc8b2826c4d0c6173cf0e6b867f44
Sungkeun Choi <soliven25@gmail.com>
2016-04-26 14:15:38 +0900

    first Commit

---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sphere : MonoBehaviour {
+	
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 0.06F;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float leftRight = Input.GetAxis("Horizontal");
+		float upDown = Input.GetAxis("Vertical");
+		
+		//Debug.Log("leftRight: " + leftRight);
+		
+		transform.Translate(Vector3.right*leftRight*speed,Space.World);
+		transform.Translate(Vector3.up*upDown*speed, Space.World);
+	
+	}
+}
---
+++ Assets/Scripts/flight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flight : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+	void Start () {
+		speed = 0.06F;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float leftRight = Input.GetAxis("Horizontal");
+		float upDown = Input.GetAxis("Vertical");
+		
+		transform.Translate(Vector3.right*speed*leftRight,Space.World);
+		transform.Translate(Vector3.up*speed*upDown, Space.World);
+		
+	}
+}
